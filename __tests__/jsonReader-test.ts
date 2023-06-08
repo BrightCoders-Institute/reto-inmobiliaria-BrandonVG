cedd29ed6b1fd73e9fcbe1ccf1d89807131ffd8d
BrandonVG <brandon.vadillox3@gmail.com>
2023-06-08 16:53:59 -0600

    Created Inmueble type and a  module to read the json local file and return an array of Inmueble type

---
+++ __tests__/jsonReader-test.ts
@@ -0,0 +1,14 @@
+import JsonReader from "../src/utils/jsonReader";
+import data from "../src/db/data.json";
+
+describe('Test for json reader',() => {
+  test('array data should have 10 items', () =>{
+    const array = JsonReader.jsonInmueblesToArray(data);
+    expect(array.length).toBe(10);
+  });
+
+  test('Should return The Willows as the name of the first item', () =>{
+    const array = JsonReader.jsonInmueblesToArray(data);
+    expect(array[0].name).toBe('The Willows');
+  });
+});
---
+++ src/utils/jsonReader.ts
@@ -0,0 +1,26 @@
+import { Inmueble } from "../types/inmuebleType";
+
+class JsonReader{
+
+  static jsonInmueblesToArray(data: any){
+    const inmueblesArray: Inmueble[] = data.map((item: any) =>{
+      const inmueble: Inmueble = {
+        id: item.id,
+        name: item.name,
+        address: item.address,
+        rooms: item.rooms,
+        bathrooms: item.bathrooms,
+        size: item.size,
+        price: item.price,
+        stars: item.stars,
+        like: item.like,
+        img: item.img,
+      };
+      return inmueble;
+    });
+    return inmueblesArray;
+  }
+
+}
+
+export default JsonReader;
