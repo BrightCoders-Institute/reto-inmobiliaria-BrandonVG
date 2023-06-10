23f9ea1472bdf9115a15d53b06f73d58e0cccb68
BrandonVG <brandon.vadillox3@gmail.com>
2023-06-10 16:37:33 -0600

    Created itemcard component
    Created itemcard component that shows the info from a house, it still dont shows
    the house stars, because the image and rate will be separated to a new component

--- App.tsx
+++ App.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import {View} from 'react-native';
-
+import {SafeAreaView, StyleSheet, Platform, NativeModules} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import ItemCard from './src/components/itemCard';
+import data from "./src/db/data.json"
+import JsonReader from './src/utils/jsonReader';
 
 
 
 function App(): JSX.Element {
-
+  const inmuebles = JsonReader.jsonInmueblesToArray(data);
   return (
-    <View>
-      
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style='auto'/>
+      <ItemCard inmueble={inmuebles[0]}/>
+    </SafeAreaView>
   );
 }
 
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#FFFFFF',
+    paddingTop: Platform.OS === 'android' ? NativeModules.StatusBarManager.HEIGHT : 0,
+    alignItems:'center',
+  },
+});
 
 export default App;
